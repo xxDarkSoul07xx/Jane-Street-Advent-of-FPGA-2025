a93e8de598185dbc7d94f2759dbd7fb79e438fd8
Anirudh_Bhaskar <baniru007@gmail.com>
2025-12-15 22:11:02 -0500

    added rest of the fsm stuff and fixed up some other errors

--- Design1/rtl/sequential.sv
+++ Design1/rtl/sequential.sv
@@ -19,64 +19,97 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+// figure out how to read files
+// distance and possibly direction will be affected?
 module design1(
     input logic clk,
     input logic rst_n,
     input logic valid,
-    input logic direction,
-    input logic [15:0] distance,
+    input logic direction, // 1 is right, 0 is left
+    input logic [15:0] distance, // 16 bits should be enough for how far left or right to move
     output logic ready,
-    output logic [15:0] zero_count
+    output logic [15:0] zero_count // same here 16 bits should be enough
     );
     
-    logic [6:0] position;
-    logic [17:0] temp_pos;
+    logic [6:0] position; // 0-99 so we need 7 bits to represent them
+    logic signed [17:0] temp_pos; // temp holder for the position (to help deal with if our position after moving the distance is >= 100 or < 0)
+    logic direction_reg; // copy over the direction and distance from inputs for internal use in case original inputs change
+    logic [15:0] distance_reg;
     
-    typedef enum logic [2:0] {idle, extract, calculate, edit, check} statetype;
+    typedef enum logic [2:0] {idle, extract, calculate, edit, check} statetype; // states for the fsm
     statetype state, nextstate;
-    
+
     always_ff @(posedge clk or negedge rst_n) begin
-        if (rst_n) begin
+        if (!rst_n) begin // reset: state is idle, initial position on dial is 50, set zero_count to 0, and ready to 0
             state <= idle;
+            position <= 7'd50;
+            zero_count <= 16'd0;
+            ready <= 1'b0;
         end else begin
-            state <= nextstate;
+            state <= nextstate; // if not reset, go to the next state
         end
     end
     
     always_comb begin
-        nextstate = state;
+        nextstate = state; // by default, stay in the current state
         case (state)
             idle: begin
                 if (valid) nextstate = extract;
-                else nextstate = idle;
             end
-            extract: begin //come back to here
+            extract: begin
                 nextstate = calculate;
             end
-            calculcate: begin
+            calculate: begin
                 nextstate = edit;
             end
             edit: begin
-                if (temp_pos >= 100) begin
-                    temp_pos = temp_pos - 100;
-                    nextstate = edit;
-                end
-                if (temp_pos < 0) begin
-                    temp_pos = temp_pos + 100;
-                    nextstate = edit;
-                end else begin
-                    position = temp_pos;
+                if (temp_pos >= 100 || temp_pos < 0)
+                    nextstate = edit; // keep looping until temp_pos is between 0-99 inclusive
+                else
                     nextstate = check;
-                end
             end
-            check: begin // figure out where to go after this
-                if (position == 0) begin
-                    zero_count = zero_count + 1;
-                    //nextstate here
-                end else begin
-                    //another nextstate here
-                end
+            check: begin
+                nextstate = idle; // when done, go back to idle
             end
+            default: nextstate = idle; // by default, go back to idle
+        endcase
+    end
     
-          endmodule
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin // don't need anything since we already handled rst_n
+        end else begin
+            case (state)
+                idle: begin
+                    ready <= 1'b1; // get ready for the next data
+                end
+                extract: begin
+                    direction_reg <= direction; // copy the inputs
+                    distance_reg <= distance;
+                    ready <= 1'b0;
+                end
+                calculate: begin
+                    if (direction_reg == 1'b1) begin
+                        temp_pos <= position + distance_reg; // right
+                    end else begin
+                            temp_pos <= position - distance_reg; // left
+                    end
+                end
+                edit: begin
+                // figure out our new locations in the case that we wrap around
+                    if (temp_pos >= 100) begin
+                        temp_pos <= temp_pos - 100; // wrapped from 99 to 0
+                    end else if (temp_pos < 0) begin
+                        temp_pos <= temp_pos + 100; // wrapped from 0 to 99
+                    end else begin
+                        position <= temp_pos[6:0]; // if in range, update our position
+                    end
+                end
+                check: begin
+                    if (position == 7'd0) begin // check if our position is at 0
+                        zero_count <= zero_count + 16'd1; // if it is, add one to zero_count
+                    end
+                end
+            endcase
+        end
+    end                         
+endmodule
